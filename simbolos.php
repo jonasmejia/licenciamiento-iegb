2b3b24d1e6fd32d5ee57e1d59e36333bf8fc12b7
stelr <141653096+stelr@users.noreply.github.com>
2024-08-22 06:10:41 -0500

    Update simbolos.php

--- simbolos.php
+++ simbolos.php
@@ -117,7 +117,8 @@
             </div>
         </section>
 
-        <div class="container my-5">
+        <div class="row g-9">
+            <div class="col-md-8">
             <div class="row justify-content-center">
                 <div class="hexagon-container">
                     <div class="hexagon"><img src="assets/img/escudo-iegb.webp" alt="Imagen 1"></div>
@@ -178,6 +179,63 @@
                                 la excelencia de nuestra profesión.
                             </p>
                         </div>
+                    </div>
+                </div>
+            </div>
+
+            </div>
+
+            <div class="col-md-4">
+                <div class="position-sticky" style="top: 2rem;">
+                    <div class="p-4 mb-3 bg-body-tertiary rounded">
+                        <h4 class="fst-italic">Avisos o texto porseacaso</h4>
+                        <p class="mb-0">Customize this section to tell your visitors a little bit about your publication, writers,
+                            content, or something else entirely. Totally up to you.</p>
+                    </div>
+
+                    <div>
+                        <h4 class="fst-italic">Noticias</h4>
+                        <ul class="list-unstyled">
+                            <li>
+                                <a class="d-flex flex-column flex-lg-row gap-3 align-items-start align-items-lg-center py-3 link-body-emphasis text-decoration-none border-top"
+                                    href="#">
+                                    <svg class="bd-placeholder-img" width="100%" height="96" xmlns="http://www.w3.org/2000/svg"
+                                        aria-hidden="true" preserveAspectRatio="xMidYMid slice" focusable="false">
+                                        <rect width="100%" height="100%" fill="#777" />
+                                    </svg>
+                                    <div class="col-lg-8">
+                                        <h6 class="mb-0">Example blog post title</h6>
+                                        <small class="text-body-secondary">January 15, 2024</small>
+                                    </div>
+                                </a>
+                            </li>
+                            <li>
+                                <a class="d-flex flex-column flex-lg-row gap-3 align-items-start align-items-lg-center py-3 link-body-emphasis text-decoration-none border-top"
+                                    href="#">
+                                    <svg class="bd-placeholder-img" width="100%" height="96" xmlns="http://www.w3.org/2000/svg"
+                                        aria-hidden="true" preserveAspectRatio="xMidYMid slice" focusable="false">
+                                        <rect width="100%" height="100%" fill="#777" />
+                                    </svg>
+                                    <div class="col-lg-8">
+                                        <h6 class="mb-0">This is another blog post title</h6>
+                                        <small class="text-body-secondary">January 14, 2024</small>
+                                    </div>
+                                </a>
+                            </li>
+                            <li>
+                                <a class="d-flex flex-column flex-lg-row gap-3 align-items-start align-items-lg-center py-3 link-body-emphasis text-decoration-none border-top"
+                                    href="#">
+                                    <svg class="bd-placeholder-img" width="100%" height="96" xmlns="http://www.w3.org/2000/svg"
+                                        aria-hidden="true" preserveAspectRatio="xMidYMid slice" focusable="false">
+                                        <rect width="100%" height="100%" fill="#777" />
+                                    </svg>
+                                    <div class="col-lg-8">
+                                        <h6 class="mb-0">Longer blog post title: This one has multiple lines!</h6>
+                                        <small class="text-body-secondary">January 13, 2024</small>
+                                    </div>
+                                </a>
+                            </li>
+                        </ul>
                     </div>
                 </div>
             </div>
