e1c71f67216587c715e5d101b74a44c6bc82d89b
stelr <141653096+stelr@users.noreply.github.com>
2024-08-22 06:00:14 -0500

    Update plana-jerarquica.php

--- plana-jerarquica.php
+++ plana-jerarquica.php
@@ -417,7 +417,7 @@
             <div class="col-md-4">
                 <div class="position-sticky" style="top: 2rem;">
                     <div class="p-4 mb-3 bg-body-tertiary rounded">
-                        <h4 class="fst-italic">Avisos o texto porseacaso</h4>
+                        <h4 class="fst-italic">Avisos</h4>
                         <p class="mb-0">Customize this section to tell your visitors a little bit about your publication, writers,
                             content, or something else entirely. Totally up to you.</p>
                     </div>
